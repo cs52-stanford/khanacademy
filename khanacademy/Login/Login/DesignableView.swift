92ae6a61315f264b0ca617af09d3f6c96bc87a5f
Linden Li <lindenli@stanford.edu>
2020-05-09 14:03:26 -0400

    Login test
    Login screen

---
+++ khanacademy/Login/Login/DesignableView.swift
@@ -0,0 +1,10 @@
+//
+//  DesignableView.swift
+//  Login
+//
+//  Created by Linden Li on 5/6/20.
+//  Copyright © 2020 Linden Li. All rights reserved.
+//
+
+import UIKit
+
---
+++ khanacademy/Login/Login/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Linden Li on 5/6/20.
+//  Copyright © 2020 Linden Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
