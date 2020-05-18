8c60da441b97acc87f1ff57d79d34010cded4fc6
Jay Saleh <jaysaleh66@gmail.com>
2020-05-17 21:25:00 -0700

    merged everything and added all the files

---
+++ khanacademy/khanacademy/MapViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MapViewController.swift
+//  khanacademy
+//
+//  Created by Jay Saleh on 4/30/20.
+//  Copyright © 2020 cs52. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import GooglePlaces
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    var resultsViewController: GMSAutocompleteResultsViewController?
+    var searchController: UISearchController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSearchController()
+    }
+
+    func setupSearchController() {
+        resultsViewController = GMSAutocompleteResultsViewController()
+        //resultsViewController?.delegate = self
+        searchController = UISearchController(searchResultsController: resultsViewController)
+        searchController?.searchResultsUpdater = resultsViewController
+
+        let searchBar = searchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = searchController?.searchBar
+        definesPresentationContext = true
+        searchController?.hidesNavigationBarDuringPresentation = false
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ViewController: GMSAutocompleteResultsViewControllerDelegate {
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController, didAutocompleteWith place: GMSPlace) {
+        
+    }
+    
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController, didFailAutocompleteWithError error: Error) {
+        
+    }
+}
---
+++ khanacademy/khanacademy/SignUpViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignUpViewController.swift
+//  Login
+//
+//  Created by Linden Li on 5/9/20.
+//  Copyright © 2020 Linden Li. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+}
